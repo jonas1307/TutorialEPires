f1e82266de9669e6b34b3c7c18c97d305da89264
Jonas Lima <jonas1307@live.com>
2017-12-25 22:36:52 -0200

    Entity + Migrations

---
+++ CadastroClientes.Infrastructure.Data/Context/CadastroClientesContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using CadastroClientes.Domain.Entities;
+
+namespace CadastroClientes.Infrastructure.Data.Context
+{
+    public class CadastroClientesContext : DbContext
+    {
+        public CadastroClientesContext()
+            : base("CadastroClientes")
+        { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
