d4cd64d38d7896238308ea5b30bfc10d81324232
Jonas Lima <jonas1307@live.com>
2017-12-25 22:50:15 -0200

    Modificações Context

--- CadastroClientes.Infrastructure.Data/Context/CadastroClientesContext.cs
+++ CadastroClientes.Infrastructure.Data/Context/CadastroClientesContext.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
 using CadastroClientes.Domain.Entities;
 
 namespace CadastroClientes.Infrastructure.Data.Context
@@ -8,6 +11,46 @@ namespace CadastroClientes.Infrastructure.Data.Context
         public CadastroClientesContext()
             : base("CadastroClientes")
         { }
+
+        #region Overrides EF
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Produto -> Produtoes
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //Evitar deletar em cascata
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            // Setar campo XptoId como ID da tabela
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            // Setar todas strings como VARCHAR
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            // Quando não houver MAXLENGHT, setar para 255 ao invés de MAX
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(255));
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+
+                if (entry.State == EntityState.Modified)
+                    entry.Property("DataCadastro").IsModified = false;
+            }
+
+            return base.SaveChanges();
+        }
+
+        #endregion
 
         public DbSet<Cliente> Clientes { get; set; }
     }
